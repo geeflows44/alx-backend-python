d8445cde005d0509a0ca223cc1bf535bc3e3a296
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2023-07-10 20:33:14 +0000

    Added new file

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python3
-
-
+"""Measures the total execution time for wait_n(n, max_delay)"""
 import asyncio
 import time
-
-
-'''From the previous file, import wait_n into 2-measure_runtime.py.
-
-    Create a measure_time function with integers n and max_delay as arguments
-    that measures the total execution time for wait_n(n, max_delay), and
-    returns total_time / n. Your function should return a float.
-
-    Use the time module to measure an approximate elapsed time.
-'''
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    '''returnsthe total time coroutines took to completion'''
-    wait_n = __import__('1-concurrent_coroutines').wait_n
-
-    start_time = time.time()
+    """Returns total_time (total execution time) / n"""
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    total_time = time.time() - start_time
-
-    return total_time / n
-
-
-if __name__ == '__main__':
-    print(measure_time(5, 3))
-    print(measure_time(5, 9))
+    end_time = time.perf_counter()
+    return (end_time - start_time) / n
